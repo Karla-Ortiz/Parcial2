64623f3b5b58aca0e037b77b54382f000b3c1580
Karla Ortiz <gabriela.ortiz@galileo.edu>
2021-04-18 20:44:48 -0600

    first commit

---
+++ Parcial2/Parcial2.cpp
@@ -0,0 +1,12 @@
+// Parcial2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+    system("pause");
+    return 0;
+}
+
