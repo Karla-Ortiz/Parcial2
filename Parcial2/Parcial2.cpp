1e32be9b609538ae100de76df5e1ae6fe77cfcab
Karla Ortiz <gabriela.ortiz@galileo.edu>
2021-04-18 20:58:20 -0600

    [ADD] Conexion

--- Parcial2/Parcial2.cpp
+++ Parcial2/Parcial2.cpp
@@ -1,11 +1,19 @@
 // Parcial2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
 
+#include <mysql.h>
 #include <iostream>
 using namespace std;
 int main()
 {
-    cout << "Hello World!\n";
+    MYSQL* conectar;
+    conectar = mysql_init(0);
+    conectar = mysql_real_connect(conectar, "localhost", "root", "root", "parcial2", 3306, NULL, 0);
+    if (conectar) {
+        cout << "Conexion exitosa..." << endl;
+    } else {
+        cout << "Error al conectar" << endl;
+    }
     system("pause");
     return 0;
 }
